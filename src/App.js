76aac2c571930dd08ae672bb7c77b0ea0d299841
George Kandalaft <george.kandalaft@mail.mcgill.ca>
2021-02-19 23:18:37 -0500

    added collection preview pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import HomePage from "./pages/home/HomePage";
+import ShopPage from "./pages/shop/ShopPage";
+
+import { Route, BrowserRouter as Router } from "react-router-dom";
+
+import "./App.scss";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Route exact path="/" component={HomePage} />
+      <Route path="/shop" component={ShopPage} />
+    </Router>
   );
 }
 
---
+++ src/pages/shop/ShopPage.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+
+import CollectionPreview from './../../components/collectionPreview/CollectionPreview'
+import SHOP_DATA from './data-shop.js';
+
+const ShopPage = () => {
+    const [shopData,] = useState(SHOP_DATA);
+
+    shopData.map((e) => console.log(e));
+
+    return (
+        <div>
+            {shopData.map(({ id, ...other }) => (
+                <CollectionPreview key={id} {...other} />
+            ))}
+        </div>
+    )
+}
+
+export default ShopPage;
