10574ae66d0bc428ef322b17f945b486b58b54cd
George Kandalaft <george.kandalaft@mail.mcgill.ca>
2021-02-20 10:35:14 -0500

    fixed import

--- src/pages/shop/ShopPage.jsx
+++ src/pages/shop/ShopPage.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 
-import CollectionPreview from './../../components/collectionPreview/CollectionPreview'
+import CollectionPreview from '../../components/collection-preview/CollectionPreview'
 import SHOP_DATA from './data-shop.js';
 
 const ShopPage = () => {
